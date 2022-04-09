a8d4dbca339214617e3ea630cfdd866a3592d313
Blackerberg <77932547+Blackerberg@users.noreply.github.com>
2022-04-09 02:46:35 +0100

    Update 100-print_comb3.c
    print 100comb3 update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
 * main - Prints numbers between 00 to 89.
 *
@@ -9,61 +7,28 @@
 */
 
 int main(void)
-
 {
-
-	int i, e;
-
-
-
-	i = 48;
-
-	e = 48;
-
-
-
-	while (e < 58)
-
-	{
-
-		i = 48;
-
-		while (i < 58)
-
+int i, e;
+i = 48;
+e = 48;
+while (e < 58)
+	{		i = 48;
+	while (i < 58)
 		{
-
 			if (e != i && e < i)
-
-			{
-
-				putchar(e);
-
-				putchar(i);
-
+			{				putchar(e);
+	putchar(i);
 				if (i == 57 && e == 56)
-
 				{
-
 					break;
-
 				}
-
 				putchar(',');
-
 				putchar(' ');
-
 			}
-
 			i++;
-
 		}
-
-		e++;
-
+e++;
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
